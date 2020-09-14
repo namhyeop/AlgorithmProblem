1028794a53edb85227bbfb44f84ab66bc81b6525
NamHyeop Kim <skaguq@gmail.com>
2020-09-15 01:35:57 +0900

    Update 5430번(AC).cpp

--- 백준알고리즘/자료구조/5430번(AC).cpp
+++ 백준알고리즘/자료구조/5430번(AC).cpp
@@ -4,17 +4,10 @@
 #include<string>
 using namespace std;
 
-void rev(deque<string> & d)
-{
-	string temp;
-	for (deque<string>::size_type i = 0; i < d.size() / 2; i++)
-	{
-		temp = d[i];
-		d[i] = d[d.size() - i - 1];
-		d[d.size() - i - 1] = temp;
-	}
-}
-
+//진짜 눈물이 앞을 가리는 문제다
+//투자한 시간 때문에 평생 기억남을 문제 
+//3시~ 1시32분 종료. deque STL 덕분에 제대로 이해하고 간다..
+//다시 한 번 멍청한 두뇌에 감탄한다.
 int main()
 {
 	int Testcase;
@@ -30,16 +23,16 @@ int main()
 		cin >> comds;
 		cin >> Asize;
 		cin >> inputA;
-		
-		for (int i = 0; i < comds.size(); i++)
+
+		for (int i = 0; i < comds.size(); i++) //q에는 명령 함수를 담아준다
 			q.push(comds[i]);
 
-		for (int i = 0; i < inputA.size(); i++)
+		for (int i = 0; i < inputA.size(); i++)//문자 파싱과정. 숫자들만 담아줄것이다.
 		{
 			if (inputA[i] == '[')
 			{
 				i++;
-				string ret ="";
+				string ret = "";
 				while (inputA[i] != ',' && inputA[i] != ']')
 				{
 					ret += inputA[i];
@@ -49,7 +42,7 @@ int main()
 			}
 			else
 			{
-				string ret="";
+				string ret = "";
 				while (inputA[i] != ',' && inputA[i] != ']')
 				{
 					ret += inputA[i];
@@ -59,50 +52,65 @@ int main()
 			}
 		}
 
-		cout << dq.front() << endl;
-		if (dq.empty())
+		/*
+		if (dq.front() == "" || dq.back() == "")
 		{
 			cout << "error" << endl;
 			break;
-		}
+		}*/
 
-		int error_sign = 0;
+		bool reverse = false; //함수가 뒤집어진 상태인지를 구분하는 flag
+		int error_sign = 0;// error가 발생했는지 안 발생했는지 구분하는 flag, error시 파싱 문자를 출력을 안해야하기 때문에 존재
 		while (!q.empty())
 		{
 			char command = q.front();
 			q.pop();
 
-			if (command == 'D' && dq.empty())
+			if (command == 'D' && (dq.empty() || dq.front() =="")) //함수가 비었는데 D명령시 오류 발생
 			{
 				cout << "error" << endl;
 				error_sign = 1;
 				break;
 			}
-
+			
 			switch (command)
 			{
 			case 'R':
-				rev(dq);
+				reverse = !reverse;
 				break;
 			case 'D':
-				dq.pop_front();
+				if (reverse == false) //함수가 뒤집혀지지 않으면 앞에서 pop
+					dq.pop_front();
+				else
+					dq.pop_back();//함수가 뒤집혀있으면 뒤에서 pop
 				break;
 			}
 		}
 
 		if (error_sign != 1)
-		cout << "[";
+			cout << "[";
 
 		while (error_sign != 1 && !dq.empty())
-		{
-			string alpha = dq.front();
-			dq.pop_front();
-			cout << alpha;
-			if (dq.size() != 0)
-				cout << ",";
+		{	
+			if (reverse == false) //함수가 뒤집혀지지 않을 경우 앞에서 부터 출력
+			{
+				string alpha = dq.front();
+				dq.pop_front();
+				cout << alpha;
+				if (dq.size() != 0)
+					cout << ",";
+			}
+			else
+			{
+				string alpha = dq.back();//함수가 뒤집혀져있을 경우 뒤에서부터 출력
+				dq.pop_back();
+				cout << alpha;
+				if (dq.size() != 0)
+					cout << ",";
+			}
 		}
 		if (error_sign != 1)
-		cout << "]" << endl;
+			cout << "]" << endl;
 	}
 
 	return 0;
