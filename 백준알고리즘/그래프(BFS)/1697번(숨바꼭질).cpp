63804a7731df246a08a80acc77437a565ed2a5bf
NamHyeop Kim <skaguq@gmail.com>
2021-01-05 15:18:34 +0900

    Create 1697번(숨바꼭질).cpp

---
+++ 백준알고리즘/그래프(BFS)/1697번(숨바꼭질).cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+const int MAX = 100000 + 1;
+bool visited[MAX];
+
+int minSecond(int N, int K)
+{
+	queue<pair<int, int>> q;
+
+	q.push(make_pair(N, 0));
+	visited[N] = true;
+
+	while (!q.empty())
+	{
+		int curLoc = q.front().first;
+		int curSec = q.front().second;
+		q.pop();
+
+		if (curLoc == K)
+			return curSec;
+
+		if (curLoc + 1 < MAX && !visited[curLoc + 1])
+		{
+			q.push(make_pair(curLoc + 1, curSec + 1));
+			visited[curLoc + 1] = true;
+		}
+		if (curLoc - 1 >= 0 && !visited[curLoc - 1])
+		{
+			q.push(make_pair(curLoc - 1, curSec + 1));
+			visited[curLoc - 1] = true;
+		}
+		if (curLoc * 2 < MAX && !visited[curLoc * 2])
+		{
+			q.push(make_pair(curLoc * 2, curSec + 1));
+			visited[curLoc * 2] = true;
+		}
+	}
+}
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+
+	cout << minSecond(N, K) << "\n";
+
+	return 0;
+}
