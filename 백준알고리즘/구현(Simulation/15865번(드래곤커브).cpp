2eb96a83e337ddd3d43b0868ec228611492d731f
NamHyeop Kim <skaguq@gmail.com>
2021-02-15 16:32:01 +0900

    Create 15865번(드래곤커브).cpp

---
+++ 백준알고리즘/구현(Simulation/15865번(드래곤커브).cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define MAX 101
+using namespace std;
+
+int moveY[] = { 0, -1, 0, 1 };
+int moveX[] = { 1, 0, -1, 0 };
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	int x, y, d, g;
+	int board[MAX][MAX] = { 0 };
+	int ret = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x >> y >> d >> g;
+
+		vector<int> dir;
+		dir.push_back(d);
+	
+		for (int i = 1; i <= g; i++)
+		{
+			int len = (int)dir.size() - 1;
+			for (int j = len; j >= 0; j--)
+				dir.push_back((dir[j] + 1) % 4);
+		}
+
+		board[y][x]++;
+		for (int i = 0; i < dir.size(); i++)
+		{
+			y = y + moveY[dir[i]];
+			x = x + moveX[dir[i]];
+			board[y][x]++;
+		}
+	}
+  
+  //정사각형 유무를 판단
+	for (int i = 0; i < 100; i++)
+	for (int j = 0; j < 100; j++)
+	{
+		if (board[i][j] && board[i][j + 1] && board[i + 1][j] && board[i + 1][j + 1])
+			ret++;
+	}
+	
+	cout << ret << "\n";
+
+	return 0;
+}
