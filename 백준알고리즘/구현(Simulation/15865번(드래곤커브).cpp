533d1eaa7e64c8b38abed103286939435f9d9f63
NamHyeop Kim <skaguq@gmail.com>
2021-02-15 16:53:14 +0900

    Update 15865번(드래곤커브).cpp

--- 백준알고리즘/구현(Simulation/15865번(드래곤커브).cpp
+++ 백준알고리즘/구현(Simulation/15865번(드래곤커브).cpp
@@ -2,25 +2,21 @@
 #define MAX 101
 using namespace std;
 
+int n,x,y,d,g;
 int moveY[] = { 0, -1, 0, 1 };
 int moveX[] = { 1, 0, -1, 0 };
 
+bool board[MAX][MAX];
 int main(void)
 {
-	int n;
 	cin >> n;
-
-	int x, y, d, g;
-	int board[MAX][MAX] = { 0 };
-	int ret = 0;
-
+	
 	for (int i = 0; i < n; i++)
 	{
 		cin >> x >> y >> d >> g;
 
 		vector<int> dir;
 		dir.push_back(d);
-	
 		for (int i = 1; i <= g; i++)
 		{
 			int len = (int)dir.size() - 1;
@@ -28,24 +24,23 @@ int main(void)
 				dir.push_back((dir[j] + 1) % 4);
 		}
 
-		board[y][x]++;
+		board[y][x] = true;
+
 		for (int i = 0; i < dir.size(); i++)
 		{
 			y = y + moveY[dir[i]];
 			x = x + moveX[dir[i]];
-			board[y][x]++;
+			board[y][x] = true;
 		}
 	}
-  
-  //정사각형 유무를 판단
+
+	int ret = 0;
 	for (int i = 0; i < 100; i++)
 	for (int j = 0; j < 100; j++)
 	{
-		if (board[i][j] && board[i][j + 1] && board[i + 1][j] && board[i + 1][j + 1])
+		if (board[i][j] && board[i + 1][j] && board[i][j + 1] && board[i + 1][j + 1])
 			ret++;
 	}
-	
-	cout << ret << "\n";
 
-	return 0;
+	cout << ret << "\n";
 }
