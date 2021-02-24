dfd212cc39b3d5b13309f7b77b15cabb06961995
NamHyeop Kim <skaguq@gmail.com>
2021-02-24 10:19:15 +0900

    Update 17144번(미세먼지안녕).cpp

--- 백준알고리즘/구현(Simulation/17144번(미세먼지안녕).cpp
+++ 백준알고리즘/구현(Simulation/17144번(미세먼지안녕).cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#define MAX 50
+#define MAX 55
 using namespace std;
 
 int R, C, T;
@@ -9,25 +9,23 @@ int visited[MAX][MAX];
 vector<int> aircleaner;
 int moveY[] = { -1, 1, 0, 0 };
 int moveX[] = { 0, 0, -1, 1 };
-
-
 /*
 3 3 1
 0 30 7
 -1 10 0
 -1 0 20
 */
-void spread()
+void spread() //미세먼지 확장
 {
+	memset(srBoard, 0, sizeof(srBoard));
 	for (int i = 0; i < R; i++)
 	for (int j = 0; j < C; j++)
 	{
-		if (board[i][j] > 0)
+		if (board[i][j] >=  5)
 		{
 			srBoard[i][j] = board[i][j] / 5;
 		}
 	}
-
 	for (int i = 0; i < R; i++)
 	for (int j = 0; j < C; j++)
 	{
@@ -43,44 +41,58 @@ void spread()
 		}
 		board[i][j] -= cnt * srBoard[i][j];
 	}
-	
-	for (int i = 0; i < R; i++)
-	{
-		for (int j = 0; j < C; j++)
-		{
-			cout << board[i][j] << " ";
-		}
-		cout << "\n";
-	}
-
 }
 
-void circulation1()
+void circulation1() //시계 방향 회전
 {
 	int y = aircleaner[0];
-	 
-	for (int i = y - 1; i > 0; i++)
+
+	for (int i = y - 1; i > 0; i--)
+	{
 		board[i][0] = board[i - 1][0];
+		board[i - 1][0] = 0;
+	}
 	for (int i = 0; i < C - 1; i++)
+	{
 		board[0][i] = board[0][i + 1];
+		board[0][i + 1] = 0;
+	}
 	for (int i = 0; i < y; i++)
+	{
 		board[i][C - 1] = board[i + 1][C - 1];
+		board[i + 1][C - 1] = 0;
+	}
 	for (int i = C - 1; i > 1; i--)
+	{
 		board[y][i] = board[y][i - 1];
+		board[y][i - 1] = 0;
+	}
 }
 
-void circulation2()
+void circulation2() //반 시계 방향 회전
 {
 	int y = aircleaner[1];
 
 	for (int i = y + 1; i < R; i++)
+	{
 		board[i][0] = board[i + 1][0];
+		board[i + 1][0] = 0;
+	}
 	for (int i = 0; i < C - 1; i++)
-		board[R][i] = board[R][i + 1];
+	{
+		board[R - 1][i] = board[R - 1][i + 1];
+		board[R - 1][i + 1] = 0;
+	}
 	for (int i = R - 1; i > y; i--)
+	{
 		board[i][C - 1] = board[i - 1][C - 1];
+		board[i - 1][C - 1] = 0;
+	}
 	for (int i = C - 1; i > 1; i--)
+	{
 		board[y][i] = board[y][i - 1];
+		board[y][i - 1] = 0;
+	}
 }
 
 int main(void)
@@ -95,32 +107,11 @@ int main(void)
 			aircleaner.push_back(i);
 	}
 
-	cout << aircleaner[0] << "\n";
-	cout << aircleaner[1] << "\n";
-
 	for (int i = 0; i < T; i++)
 	{
 		spread();
 		circulation1();
-		cout << "---------" << "\n";
-		for (int i = 0; i < R; i++)
-		{
-			for (int j = 0; j < C; j++)
-			{
-				cout << board[i][j] << " ";
-			}
-			cout << "\n";
-		}
 		circulation2();
-		cout << "---------" << "\n";
-		for (int i = 0; i < R; i++)
-		{
-			for (int j = 0; j < C; j++)
-			{
-				cout << board[i][j] << " ";
-			}
-			cout << "\n";
-		}
 	}
 
 	int sum = 0;
