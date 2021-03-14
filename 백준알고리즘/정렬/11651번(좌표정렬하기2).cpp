86b0fe3bc41de8fe678005c59995aacab5a89365
NamHyeop Kim <skaguq@gmail.com>
2021-03-14 15:04:15 +0900

    Create 11651번(좌표정렬하기2).cpp

---
+++ 백준알고리즘/정렬/11651번(좌표정렬하기2).cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second < b.second)
+		return true;
+	else if (a.second == b.second)
+	{
+		if (a.first < b.first)
+			return true;
+	}
+	return false;
+}
+
+int main(void)
+{
+	cin >> n;
+
+	v.resize(n, { 0, 0 });
+	for (int i = 0; i < n; i++)
+		cin >> v[i].first >> v[i].second;
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < n; i++)
+		cout << v[i].first << " " << v[i].second << "\n";
+
+}
