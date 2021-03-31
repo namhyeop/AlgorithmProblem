4895cd78c4e587a9ca31ced9eda630e29003ed40
NamHyeop Kim <skaguq@gmail.com>
2021-04-01 00:01:32 +0900

    Update 2217번(로프).cpp

--- 백준알고리즘/그리디알고리즘/2217번(로프).cpp
+++ 백준알고리즘/그리디알고리즘/2217번(로프).cpp
@@ -1,3 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const int MAX = 1e5 + 1;
+
+int n;
+int arr[MAX];
+
+int main(void)
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+	int ret = 0;
+	for (int i = 1; i <= n; i++)
+		ret = max(ret, arr[n - i] * i);
+	cout << ret;
+}
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -28,4 +50,4 @@ int main(int argc, char* argv[])
 	cout << max << endl;
 		
 
-}
+}*/
