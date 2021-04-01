34b0f34f2c336aac9fd575408f32373a56d4b826
NamHyeop Kim <skaguq@gmail.com>
2021-04-01 10:49:48 +0900

    Update 11399(ATM).cpp

--- 백준알고리즘/그리디알고리즘/11399(ATM).cpp
+++ 백준알고리즘/그리디알고리즘/11399(ATM).cpp
@@ -1,3 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const int MAX = 1001;
+
+int arr[MAX];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	
+	sort(arr, arr + n);
+
+	int ret = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int tmpSum = 0;
+		for (int j = 0; j <= i; j++)
+		{
+			tmpSum += arr[j];
+		}
+		ret += tmpSum;
+	}
+	cout << ret << "\n";
+	
+	return 0;
+}
+
+/*
 #include"stdafx.h"
 #include<iostream>
 using namespace std;
@@ -37,3 +70,4 @@ int main(int argc, char argv[])
 		
 		printf("%d", tmp1);
 }
+*/
