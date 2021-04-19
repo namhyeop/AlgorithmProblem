b7809b0c6961a236af8c6a9dde11b66a6cd05255
NamHyeop Kim <skaguq@gmail.com>
2021-04-19 17:55:40 +0900

    Update 11404번(플로이드).cpp

--- 백준알고리즘/플로이드와샬(Floyd-Warshall)/11404번(플로이드).cpp
+++ 백준알고리즘/플로이드와샬(Floyd-Warshall)/11404번(플로이드).cpp
@@ -4,20 +4,21 @@
 #define ll long long
 
 using namespace std;
-const int MAX = 100;
+const int MAX = 101;
+const int INF = 123456789;
 int dist[MAX][MAX];
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	
+
 	int n, m;
 	cin >> n >> m;
 
 	for (int i = 0; i < n; i++)
 	for (int j = 0; j < n; j++)
-		dist[i][j] = i == j ? 0 : 1e9;
+		dist[i][j] = i == j ? 0 : INF;
 
 	for (int i = 0; i < m; i++)
 	{
@@ -34,7 +35,13 @@ int main(void)
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
+		{
+			if (dist[i][j] >= INF)
+				dist[i][j] = 0;
 			cout << dist[i][j] << " ";
+		}
 		cout << "\n";
 	}
+
+	return 0;
 }
