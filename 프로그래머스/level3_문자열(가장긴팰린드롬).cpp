72b4a9cef1671051977b5fba5dd3af86bd7c4fbe
NamHyeop Kim <skaguq@gmail.com>
2021-04-23 17:24:59 +0900

    Update level3_문자열(가장긴팰린드롬).cpp

--- 프로그래머스/level3_문자열(가장긴팰린드롬).cpp
+++ 프로그래머스/level3_문자열(가장긴팰린드롬).cpp
@@ -5,37 +5,40 @@
 
 using namespace std;
 
-int solution(string s){
-
+int solution(string s)
+{
 	int answer = 1;
 
-	for (size_t i = 1; i <= s.size(); i++){
-		int left = i - 1, right = i + 1, count_1 = 0, count_2 = 1;
-		bool f1 = false, f2 = false;
+	for (int i = 1; i <= s.size(); i++)
+	{
+		int left = i - 1, right = i + 1;
+		int evenRet = 0, oddRet = 1;
 
-	/*	cout << "======================" << "\n";
-		cout << "left : " << left << ", right : " << right << '\n';*/
-		while (left >= 0 && right <= s.size()){
-			if (s[left] == s[right - 1]) count_1 += 2;
-			else f1 = true;
+		bool flag1 = false, flag2 =false;
+		while (left >= 0 && right <= s.size())
+		{
+			if (s[left] == s[right - 1])
+				evenRet += 2;
+			else
+				flag1 = true;
 
-			/*cout << "s[left] : " << s[left] << ", s[right -1] : " << s[right - 1] << ",count_1 : " << count_1 << "\n";*/
-		
-			if (s[left] == s[right]) count_2 += 2;
-			else f2 = true;
-			//cout << "s[left] : " << s[left] << ", s[right] : " << s[right] << ",count_2 : " << count_2 << "\n";
-			left--, right++;
-			if (f1 && f2) break;
+			if (s[left--] == s[right++])
+				oddRet += 2;
+			else
+				flag2 = true;
+
+			if (flag1 && flag2)
+				break;
 		}
 
-
-		int result = (count_1 > count_2) ? count_1 : count_2;
-		answer = (result > answer) ? result : answer;
-		/*cout << "answer : " << answer << "\n";
-		cout << "======================" << "\n";*/
+		int ret = evenRet > oddRet ? evenRet : oddRet;
+		
+		if (answer < ret)
+			answer = ret;
 	}
 	return answer;
 }
+
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
