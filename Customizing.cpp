92e7d1dae73232ea9748ce6f41ce1d89e72ee90f
NamHyeop Kim <skaguq@gmail.com>
2021-04-24 23:08:40 +0900

    Update Customizing.cpp

--- Customizing.cpp
+++ Customizing.cpp
@@ -2,12 +2,13 @@
 #define pill pair<int,int>
 #define mp(X,Y) make_pair(X,Y)
 #define ll long long
+#define sz(v) (int)(v).size()
 
 using namespace std;
 
 int main(void)
 {
-ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 }
 
