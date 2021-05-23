238c3c135c5dbec2d3ff31481f9ba74e2b8bc2ad
NamHyeop Kim <skaguq@gmail.com>
2021-05-23 22:26:42 +0900

    Update Customizing.cpp

--- Customizing.cpp
+++ Customizing.cpp
@@ -2,6 +2,7 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define pill pair<int,int>
 #define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y,Z) make_tuple(X,Y,Z)
 #define ll long long
 #define sz(v) (int)(v).size()
 
