8e55e75fbc3d1fe74e00e6440969602b948dea49
NamHyeop Kim <skaguq@gmail.com>
2021-05-27 22:23:52 +0900

    Create 1753번(최단경로).cpp

---
+++ 백준알고리즘/다익스트라알고리즘(Dijkstra's Algorithm)/1753번(최단경로).cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pill pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int INF = 2123002300;
+const int MAX = 20001;
+vector<int> DIST;
+int V, E, K;
+vector<pair<int, int>> board[MAX];
+
+int main(void)
+{
+	fastio;
+
+	auto dijkstra = [&](int start) -> void
+	{
+		DIST.resize(V + 1, INF);
+		priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		pq.push({ 0, start });
+		DIST[start] = 0;
+
+		while (!pq.empty())
+		{
+			int cost = pq.top().first;
+			int cur = pq.top().second;
+			pq.pop();
+
+			for (auto next : board[cur])
+			{
+				int neighbor = next.second;
+				int neighborCost = next.first + cost;
+
+				if (DIST[neighbor] > neighborCost)
+				{
+					DIST[neighbor] = neighborCost;
+					pq.push({ neighborCost, neighbor });
+				}
+			}
+		}
+	};
+
+	cin >> V >> E >> K;
+	
+	for (int i = 0; i < E; i++)
+	{
+		int stnode, ednode, d;
+		cin >> stnode >> ednode >> d;
+		board[stnode].push_back({ d, ednode });
+	}
+
+	dijkstra(K);
+
+	for (int i = 1; i <= V; i++)
+	{
+		if (DIST[i] == INF)
+		{
+			cout << "INF" << "\n";
+			continue;
+		}
+		cout << DIST[i] << "\n";
+	}
+	return 0;
+}
