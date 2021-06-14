2f637e760b0baea0b64efec9e7d14797add27f1c
NamHyeop Kim <skaguq@gmail.com>
2021-06-14 13:13:43 +0900

    Update 15684번(사디리조작).cpp

--- 백준알고리즘/구현(Simulation/15684번(사디리조작).cpp
+++ 백준알고리즘/구현(Simulation/15684번(사디리조작).cpp
@@ -1,4 +1,84 @@
 #include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pill pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 31;
+int board[MAX][11];
+int n, m, h;
+int boardCnt;
+bool flag;
+
+int main(void)
+{
+	fastio;
+	cin >> n >> m >> h;
+
+	for (int i = 0; i < m; i++)
+	{
+		int y, x;
+		cin >> y >> x;
+		board[y - 1][x] = true;
+	}
+
+	function<void(int, int)> DFS = [&](int y, int cnt)
+	{
+		if (flag)
+			return;
+
+		if (boardCnt == cnt)
+		{
+			bool possibleflag = true;
+			for (int i = 1; i <= n; i++)
+			{
+				int row = i;
+				for (int j = 0; j < h; j++)
+				if (board[j][row])
+					row++;
+				else if (1 < row && board[j][row -1])
+					row--;
+				
+				if (i != row)
+				{
+					possibleflag = false;
+					break;
+				}
+			}
+			if (possibleflag)
+				flag = true;
+			return;
+		}
+
+		for (int i = y; i < h; i++) //순서대로 한 개씩 사디리를 놓아보는 과정 
+		for (int j = 1; j < n; j++)
+		if (!board[i][j - 1] && !board[i][j] && !board[i][j + 1])
+		{
+			board[i][j] = true;
+			DFS(i, cnt + 1);
+			board[i][j] = false;
+		}
+		return;
+	};
+
+	for (int i = 0; i <= 3; i++)
+	{
+		boardCnt = i;
+		DFS(0, 0);
+		if (flag)
+		{
+			cout << boardCnt << "\n";
+			return 0;
+		}
+	}
+	cout << -1 << "\n";
+}
+/*
+#include<bits/stdc++.h>
 #define MAX 30
 using namespace std;
 
@@ -71,3 +151,4 @@ int main(void)
 
 	cout << -1 << "\n";
 }
+*/
