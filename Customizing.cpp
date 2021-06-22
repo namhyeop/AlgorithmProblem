7838279f609ea9252bf223caa6194a58690d63da
NamHyeop Kim <skaguq@gmail.com>
2021-06-23 00:18:38 +0900

    Update Customizing.cpp

--- Customizing.cpp
+++ Customizing.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define pill pair<int,int>
+#define pii pair<int,int>
 #define mp(X,Y) make_pair(X,Y)
 #define mt(X,Y) make_tuple(X,Y)
 #define mtt(X,Y,Z) make_tuple(X,Y,Z)
