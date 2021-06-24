9c186125490916197c45a390230b1851859f5a81
NamHyeop Kim <skaguq@gmail.com>
2021-06-24 10:33:58 +0900

    Update 17825번(주사위욫놀이).cpp

--- 백준알고리즘/구현(Simulation/17825번(주사위욫놀이).cpp
+++ 백준알고리즘/구현(Simulation/17825번(주사위욫놀이).cpp
@@ -1,3 +1,99 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 33;
+
+vector<int> arr[MAX];
+int board[MAX] = {
+//0~20:외부 사이클
+0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,
+//21~23:왼쪽
+13,16,19,
+//24~25: 아래쪽
+22,24,
+//26~28: 오른쪽
+28,27,26,
+//29: 중간
+25,
+//30 ~ 31: 위
+30,35,
+//32 : 도착
+0
+};
+
+int nxt[MAX][6];
+
+int main(void)
+{
+	fastio;
+	for (int i = 1; i <= 20; i++)
+		arr[i - 1].push_back(i);
+	arr[5].push_back(21);
+	arr[21].push_back(22);
+	arr[22].push_back(23);
+	arr[23].push_back(29);
+	arr[10].push_back(24);
+	arr[24].push_back(25);
+	arr[25].push_back(29);
+	arr[15].push_back(26);
+	arr[26].push_back(27);
+	arr[27].push_back(28);
+	arr[28].push_back(29);
+	arr[29].push_back(30);
+	arr[30].push_back(31);
+	arr[31].push_back(20);
+	arr[20].push_back(32);
+	arr[32].push_back(32);
+
+	vector<int> v(10);
+	for (int i = 0; i < 10; i++)
+		cin >> v[i];
+
+	auto calculator = [&](int i, int cnt)
+	{
+		i = arr[i][i == 5 || i == 10 || i == 15];
+		cnt--;
+		while (cnt)
+		{
+			i = arr[i][0];
+			cnt--;
+		}
+		return i;
+	};
+
+	for (int i = 0; i <= 32; i++)
+	for (int j = 1; j <= 5; j++)
+		nxt[i][j] = calculator(i, j);
+
+	function<int(int, int, int, int, int)> DFS = [&](int a, int b, int c, int d, int idx) -> int
+	{
+		if (idx == 10)
+			return 0;
+		int ret = 0;
+		int nextA = nxt[a][v[idx]];
+		int nextB = nxt[b][v[idx]];
+		int nextC = nxt[c][v[idx]];
+		int nextD = nxt[d][v[idx]];
+		if (a != 32 && (nextA == 32 || nextA != b && nextA != c && nextA != d))
+			ret = max(ret, DFS(nextA, b, c, d, idx + 1) + board[nextA]);
+		if (b != 32 && (nextB == 32 || nextB != a && nextB != c && nextB != d))
+			ret = max(ret, DFS(a, nextB, c, d, idx + 1) + board[nextB]);
+		if (c != 32 && (nextC == 32 || nextC != a && nextC != b && nextC != d))
+			ret = max(ret, DFS(a, b, nextC, d, idx + 1) + board[nextC]);
+		if (d != 32 && (nextD == 32 || nextD != a && nextD != b && nextD != c))
+			ret = max(ret, DFS(a, b, c, nextD, idx + 1) + board[nextD]);
+		return ret;
+	};
+	cout << DFS(0, 0, 0, 0, 0) << "\n";
+}
+/*
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -79,3 +175,4 @@ int main()
 
 	cout << ret << "\n";
 }
+*/
