311b544f7ef10d2a8b6945d978bf5d56615c6e53
NamHyeop Kim <skaguq@gmail.com>
2021-07-21 15:56:02 +0900

    Update 1927번(최소힙).cpp

--- 백준알고리즘/우선순위큐/1927번(최소힙).cpp
+++ 백준알고리즘/우선순위큐/1927번(최소힙).cpp
@@ -1,3 +1,103 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 1e5 + 1;
+vector<int> arr;
+int n;
+
+void insert(int num)
+{
+	arr.push_back(num);
+	int current = arr.size() - 1;
+	int parent = current / 2;
+	while (true)
+	{
+		if (parent == 0 || arr[parent] <= arr[current])
+		{
+			break;
+		}
+		int tmp = arr[parent];
+		arr[parent] = arr[current];
+		arr[current] = tmp;
+
+		current = parent;
+		parent = current / 2;
+	}
+};
+
+int remove()
+{
+	if (arr.size() == 1)
+	{
+		return 0;
+	}
+
+	int top = arr[1];
+	arr[1] = arr[arr.size() - 1];
+	arr.pop_back();
+
+	int currentPos = 1;
+	while (true)
+	{
+		int leftPos = currentPos * 2;
+		int rightPos = currentPos * 2 + 1;
+
+		if (leftPos >= arr.size())
+		{
+			break;
+		}
+
+		int minValue = arr[leftPos];
+		int minPos = leftPos;
+
+		if (rightPos < arr.size() && minValue > arr[rightPos])
+		{
+			minValue = arr[rightPos];
+			minPos = rightPos;
+		}
+
+		if (arr[currentPos] > minValue)
+		{
+			int tmp = arr[currentPos];
+			arr[currentPos] = arr[minPos];
+			arr[minPos] = tmp;
+			currentPos = minPos;
+		}
+		else
+			break;
+	}
+	return top;
+};
+
+int main(void)
+{
+	fastio;
+	cin >> n;
+	arr.push_back(0);
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+
+		if (num == 0)
+		{
+			cout << remove() << "\n";
+		}
+		else
+		{
+			insert(num);
+		}
+	}
+}
+
+/*
 #include<iostream>
 #include<queue>
 #include<functional>
@@ -30,3 +130,4 @@ int main()
 	}
 	return 0;
 }
+*/
