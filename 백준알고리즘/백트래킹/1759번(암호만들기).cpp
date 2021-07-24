b5853021d30281696441e95e46a5ef4f9c1fa250
NamHyeop Kim <skaguq@gmail.com>
2021-07-25 00:41:58 +0900

    Update 1759번(암호만들기).cpp

--- 백준알고리즘/백트래킹/1759번(암호만들기).cpp
+++ 백준알고리즘/백트래킹/1759번(암호만들기).cpp
@@ -1,4 +1,61 @@
 #include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 20;
+int l, c; 
+char arr[MAX];
+vector<string> ret;
+int main(void)
+{
+	fastio;
+	cin >> l >> c;
+	for (int i = 0; i < c; i++)
+		cin >> arr[i];
+	sort(arr, arr + c);
+
+	function<void(int, string, int, int)> DFS = [&](int pos, string str, int ja, int mo)
+	{
+		if (str.size() == l)
+		{
+			if (ja < 2 || mo < 1)
+				return;
+			ret.push_back(str);
+			return;
+		}
+		
+		for (int i = pos; i < c; i++)
+		{
+			if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
+			{
+				str += arr[i];
+				DFS(i + 1, str, ja, mo + 1);
+				str.pop_back();
+			}
+			else
+			{
+				str += arr[i];
+				DFS(i + 1, str, ja + 1, mo);
+				str.pop_back();
+			}
+		}
+	};
+
+	DFS(0, "", 0, 0);
+
+	for (auto ans : ret)
+		cout << ans << "\n";
+	return 0;
+}
+
+/*
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -60,3 +117,4 @@ int main(int argc, char *argv[])
 	func(0,0);
 
 }
+*/
