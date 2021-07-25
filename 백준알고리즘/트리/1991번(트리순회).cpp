fa731c41cba099ed56c5cce733b0011fadc08001
NamHyeop Kim <skaguq@gmail.com>
2021-07-25 23:06:34 +0900

    Update 1991번(트리순회).cpp

--- 백준알고리즘/트리/1991번(트리순회).cpp
+++ 백준알고리즘/트리/1991번(트리순회).cpp
@@ -1,8 +1,9 @@
+//이 방법이 훨씬 더 직접적
 #include<iostream>
 
 using namespace std;
 
-char Tree[26][2] = { '.', };
+char Tree[26][2];
 
 void preorder(char root)
 {
@@ -20,24 +21,20 @@ void inorder(char root)
 {
 	if (root == '.')
 		return;
-	else
-	{
+	
 		inorder(Tree[root - 'A'][0]);
 		cout << root;
 		inorder(Tree[root - 'A'][1]);
-	}
 }
 
 void postorder(char root)
 {
 	if (root == '.')
 		return;
-	else
-	{
+		
 		postorder(Tree[root - 'A'][0]);
 		postorder(Tree[root - 'A'][1]);
 		cout << root;
-	}
 }
 
 int main(int argc, char *argv[])
@@ -65,3 +62,73 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+
+/*
+//#include<bits/stdc++.h>
+//#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+//#define pii pair<int,int>
+//#define mp(X,Y) make_pair(X,Y)
+//#define mt(X,Y) make_tuple(X,Y)
+//#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+//#define ll long long
+//#define sz(v) (int)(v).size()
+//
+//using namespace std;
+//const int MAX = 26;
+//int n;
+////tree[i].second가 true면 왼쪽 자식 아니면 오른쪽 자식
+//vector<pair<int, bool>> tree[MAX];
+//
+//void preorder(int node)//MLR
+//{
+//	cout << (char)(node + 'A');
+//	for (int i = 0; i < tree[node].size(); i++)
+//		preorder(tree[node][i].first);
+//}
+//
+//void inorder(int node) // LMR
+//{
+//	if (tree[node].size() && tree[node][0].second) // 자식이 존재하고 왼쪽 자식이라면
+//		inorder(tree[node][0].first);
+//	cout << (char)(node + 'A');
+//	//오른쪽 자식만 존재한다면
+//	if (tree[node].size() && !tree[node][0].second)
+//		inorder(tree[node][0].first);
+//	//양쪽 자식이 다 존재하면
+//	else if (tree[node].size() == 2)
+//		inorder(tree[node][1].first);
+//}
+//
+//void postorder(int node)
+//{
+//	for (int i = 0; i < tree[node].size(); i++)
+//		postorder(tree[node][i].first);
+//	cout << (char)(node + 'A');
+//}
+//
+//int main(void)
+//{
+//	fastio;
+//	cin >> n;
+//	
+//	for (int i = 0; i < n; i++)
+//	{
+//		char a, b, c;
+//		cin >> a >> b >> c;
+//
+//		if (b != '.')
+//			tree[a - 'A'].push_back({ b - 'A', true });
+//		if (c != '.')
+//			tree[a - 'A'].push_back({ c - 'A', false });
+//	}
+//
+//	preorder(0);
+//	cout << "\n";
+//	inorder(0);
+//	cout << "\n";
+//	postorder(0);
+//	cout << "\n";
+//	return 0;
+//}
+*/
