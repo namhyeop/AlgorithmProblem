682be46c117e6a7c2759b40f260bf6a10456502d
NamHyeop Kim <skaguq@gmail.com>
2021-07-28 11:33:36 +0900

    Update 11657번(타임머신).cpp

--- 백준알고리즘/벨만포드(Bellman-Ford)/11657번(타임머신).cpp
+++ 백준알고리즘/벨만포드(Bellman-Ford)/11657번(타임머신).cpp
@@ -1,3 +1,67 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 501;
+const ll INF = 1e19;
+int n, m;
+vector<pair<int, int>> board[MAX];
+ll dist[MAX];
+int main(void)
+{
+	fastio;
+	cin >> n >> m;
+	
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		board[a-1].push_back({ c, b-1 });
+	}
+
+	bool cycleFlag = false;
+	fill(dist, dist + n, INF);
+	dist[0] = 0;
+	for (int i = 0; i < n; i++)
+	for (int j = 0; j < n; j++)
+	{
+		for (auto cur : board[j])
+		{
+			int cost = cur.first;
+			int neighbor = cur.second;
+			if (dist[j] != INF && dist[neighbor] > cost + dist[j])
+			{
+				dist[neighbor] = cost + dist[j];
+				
+				//순회를 다돌고 한 번더. 즉 N번째 순회에 값이 변경되면 사이클이 존재한다.
+				if (i == n - 1)
+					cycleFlag = true;
+			}
+		}
+	}
+
+
+	if (cycleFlag)
+		cout << -1 << "\n";
+	else
+	{
+		for (int i = 1; i < n; i++)
+		{
+			if (dist[i] != INF)
+				cout << dist[i] << "\n";
+			else
+				cout << -1 << "\n";
+		}
+	}
+		
+}
+/*
 #include<iostream>
 #include<vector>
 #include<utility>
@@ -53,3 +117,4 @@ int main(int argc, char *argv[])
 		}
 	}
 }
+*/
