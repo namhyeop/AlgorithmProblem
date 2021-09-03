e544d0ea0dd44c04ccec28541c9264701a54a808
NamHyeop Kim <skaguq@gmail.com>
2021-09-03 15:15:11 +0900

    Update 14499(주사위굴리기).cpp

--- 백준알고리즘/구현(Simulation/14499(주사위굴리기).cpp
+++ 백준알고리즘/구현(Simulation/14499(주사위굴리기).cpp
@@ -1,3 +1,161 @@
+/*
+객체지향형 코드
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X,Y) make_pair(X,Y)
+#define mt(X,Y) make_tuple(X,Y)
+#define mtt(X,Y,Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAX = 21;
+
+int board[MAX][MAX];
+int height, width, y, x, k;
+int moveY[] ={0,0,-1,1};
+int moveX[] ={1,-1,0,0};
+
+struct dice
+{
+    int n, m, k;
+    vector<int> dice_arr; //0으로 초기화 되있는지 확인
+    pair<int,int> dice_pos;
+    vector<vector<int>> board;
+    vector<int> cmd;
+    dice(int n , int m, int k) : n(n), m(m), k(k), board(n, vector<int>(m)), dice_arr(6), dice_pos({y,x}){}
+
+    void init()
+    {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cin >> board[i][j];
+            }
+        }
+
+        for(int i = 0; i < k; i++){
+            int tmp;
+            cin >> tmp;
+            cmd.push_back(tmp - 1);
+        }
+    }
+
+    void checkbottom(int bottom)
+    {
+        if(!board[dice_pos.first][dice_pos.second])
+            board[dice_pos.first][dice_pos.second] = dice_arr[bottom];
+        else{
+            dice_arr[bottom] = board[dice_pos.first][dice_pos.second];
+            board[dice_pos.first][dice_pos.second] = 0;
+        }
+    }
+
+    void move()
+    {
+        for(int i = 0 ; i < cmd.size(); i++)
+        {
+            if(cmd[i] == 0) // 동
+            {
+                dice_pos.first += moveY[cmd[i]];
+                dice_pos.second += moveX[cmd[i]];
+
+                if (dice_pos.first < 0 || dice_pos.first >= n || dice_pos.second < 0 || dice_pos.second >= m)
+                {
+                    dice_pos.first -= moveY[cmd[i]];
+                    dice_pos.second -= moveX[cmd[i]];
+                    continue;
+                }
+
+                int tmp = dice_arr[5];
+                dice_arr[5] = dice_arr[1];
+                dice_arr[1] = dice_arr[4];
+                dice_arr[4] = dice_arr[3];
+                dice_arr[3] = tmp;
+
+                checkbottom(3);
+            }
+            else if(cmd[i] == 1)// 서
+            {
+                dice_pos.first += moveY[cmd[i]];
+                dice_pos.second += moveX[cmd[i]];
+
+                if (dice_pos.first < 0 || dice_pos.first >= n || dice_pos.second < 0 || dice_pos.second >= m)
+                {
+                    dice_pos.first -= moveY[cmd[i]];
+                    dice_pos.second -= moveX[cmd[i]];
+                    continue;
+                }
+
+                int tmp = dice_arr[3];
+                dice_arr[3] = dice_arr[4];
+                dice_arr[4] = dice_arr[1];
+                dice_arr[1] = dice_arr[5];
+                dice_arr[5] = tmp;
+
+                checkbottom(3);
+            }
+            else if(cmd[i] == 2)// 남
+            {
+                dice_pos.first += moveY[cmd[i]];
+                dice_pos.second += moveX[cmd[i]];
+
+                if (dice_pos.first < 0 || dice_pos.first >= n || dice_pos.second < 0 || dice_pos.second >= m)
+                {
+                    dice_pos.first -= moveY[cmd[i]];
+                    dice_pos.second -= moveX[cmd[i]];
+                    continue;
+                }
+
+                int tmp = dice_arr[0];
+                dice_arr[0] = dice_arr[1];
+                dice_arr[1] = dice_arr[2];
+                dice_arr[2] = dice_arr[3];
+                dice_arr[3] = tmp;
+
+                checkbottom(3);
+            }
+            else//북
+            {
+                dice_pos.first += moveY[cmd[i]];
+                dice_pos.second += moveX[cmd[i]];
+
+                if (dice_pos.first < 0 || dice_pos.first >= n || dice_pos.second < 0 || dice_pos.second >= m)
+                {
+                    dice_pos.first -= moveY[cmd[i]];
+                    dice_pos.second -= moveX[cmd[i]];
+                    continue;
+                }
+
+                int tmp = dice_arr[1];
+                dice_arr[1] = dice_arr[0];
+                dice_arr[0] = dice_arr[3];
+                dice_arr[3] = dice_arr[2];
+                dice_arr[2] = tmp;
+
+                checkbottom(3);
+            }
+//            for(int i= 0; i < 6; i++)
+//                cout << dice_arr[i] <<" ";
+//            cout <<"\n";
+
+            cout << dice_arr[1] <<"\n";
+        }
+    }
+};
+
+int main(void)
+{
+    fastio;
+    cin >> height >> width >> y >> x >> k;
+
+    dice D(height, width, k);
+    D.init();
+    D.move();
+
+}
+*/
+
 #include<bits/stdc++.h>
 #define MAX 22
 using namespace std;
