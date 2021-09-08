9622471ac9b76f617ae7af6865a7aba8f528e2ea
남협 <skaguq@gmail.com>
2021-09-08 12:35:43 +0900

    change file

--- 백준알고리즘/구현(Simulation/15683번(감시).cpp
+++ 백준알고리즘/구현(Simulation/15683번(감시).cpp
@@ -1,103 +1,227 @@
 #include<bits/stdc++.h>
-#define MAX 8
-#define BLOCK 6
-using namespace std;
 
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X, Y) make_pair(X,Y)
+#define mt(X, Y) make_tuple(X,Y)
+#define mtt(X, Y, Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+/*
+4 6
+0 0 0 0 0 0
+0 0 6 0 0 0
+0 6 3 6 0 0
+0 0 6 0 0 0
+ */
+using namespace std;
+const int MAX = 9;
 int n, m;
-vector <pair<pair<int, int>, int> > input;
 int board[MAX][MAX];
 int ret = INT_MAX;
-int direction [5][4][2] =
-{
-	{ { 0, 1 } },
-	{ { 0, -1 }, { 0, 1 } },
-	{ { -1, 0 }, { 0, 1 } },
-	{ { -1, 0 }, { 0, 1 }, { 0, -1 } },
-	{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } }
-};
 
-void search(int level, int board[][MAX])
-{
-	if (input.size() == level)
-	{
-		int cnt = 0;
+vector<pair<int, int>> cctv;
 
-		for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-		{
-			if (board[i][j] == 0)
-				cnt++;
-		}
-		ret = min(ret, cnt);
-		return;
-	}
+int main(void) {
+    cout << (1 << 6) <<"\n";
+    fastio;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+            if (board[i][j] >= 1 && board[i][j] <= 5)
+                cctv.push_back({i, j});
+        }
+    }
 
-	int cctvNum = input[level].second;
-	int cycleValue;
-	int cursor;
-	switch (cctvNum)
-	{
-	case 0:
-		cycleValue = 4;
-		cursor = 1;
-		break;
-	case 2:
-		cycleValue = 4;
-		cursor = 2;
-		break;
-	case 3:
-		cycleValue = 4;
-		cursor = 3;
-		break;
-	case 1:
-		cycleValue = 2;
-		cursor = 2;
-		break;
-	default:
-		cycleValue = 1;
-		cursor = 4;
-		break;
-	}
+    //board의 안전구역을 탐핵한다.
+    auto search = [&](int b[MAX][MAX]) -> int {
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (b[i][j] == 0)
+                    cnt++;
+                return cnt;
+    };
 
-	for (int i = 0; i < cycleValue; i++)
-	{
-		int y = input[level].first.first;
-		int x = input[level].first.second;
-		
+    //y축인 경우에 0이 아니라면 음수 연산을 취한뒤 변경해주면 4방향으로 회전이 가능하다.
+    auto rotate = [&](vector<pair<int, int>> &dir) {
+        for (int i = 0; i < dir.size(); i++) {
+            if(dir[i].first != 0)
+            {
+                dir[i].first *= -1;
+                dir[i].second *= -1;
+                swap(dir[i].first, dir[i].second);
+            }
+            else
+                swap(dir[i].first, dir[i].second);
+        }
+    };
 
-		int tmpboard[MAX][MAX];
-		memcpy(tmpboard, board, sizeof(tmpboard));
-		for (int j = 0; j < cursor; j++)
-		{
-			int nextY = y + direction[cctvNum][j][0];
-			int nextX = x + direction[cctvNum][j][1];
-			while (1)
-			{
-				if (nextY >= 0 && nextY < n && nextX >= 0 && nextX < m)
-				{
-					if (tmpboard[nextY][nextX] != 6)
-						tmpboard[nextY][nextX] = '#';
-					else
-						break;
-				}
-				else
-					break;
+    //방향 값을 가지고 안전구역을 체크한다.
+    auto check = [&](int level, int tmpboard[MAX][MAX], vector<pair<int, int>> &dir) {
+        pair<int, int> pos = cctv[level];
 
-				nextY += direction[cctvNum][j][0];
-				nextX += direction[cctvNum][j][1];
-			}
-		}
+        for (int i = 0; i < dir.size(); i++) {
+            int ny = pos.first;
+            int nx = pos.second;
+            while (1) {
+                ny += dir[i].first;
+                nx += dir[i].second;
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m)
+                    break ;
+                if(tmpboard[ny][nx] == 6)
+                    break ;
+                if (tmpboard[ny][nx] == 0)
+                    tmpboard[ny][nx] = 23002300;
+            }
+        }
+    };
 
-		search(level + 1, tmpboard);
+    auto DFS = [&](int level, int b[MAX][MAX], auto &&DFS) -> void {
+        if (cctv.size() == level) {
+            ret = min(ret, search(b));
+            return;
+        }
+        int cycle = 0;
+        vector<pair<int, int>> dir;
 
-		for (int i = 0; i < cursor; i++)
-		{
-			int tmp = - 1 * direction[cctvNum][i][0];
-			direction[cctvNum][i][0] = direction[cctvNum][i][1];
-			direction[cctvNum][i][1] = tmp;
-		}
-	}
+        if (board[cctv[level].first][cctv[level].second] == 1) { //1번 CCTV인 경우
+            cycle = 4;
+            dir.push_back({0, 1});
+        } else if (board[cctv[level].first][cctv[level].second] == 2) { //2번 CCTV인 경우
+            cycle = 2;
+            dir.push_back({0, 1}), dir.push_back({0, -1});
+        } else if (board[cctv[level].first][cctv[level].second] == 3) { //3번 CCTV인 경우
+            cycle = 4;
+            dir.push_back({-1, 0}), dir.push_back({0, 1});
+        } else if (board[cctv[level].first][cctv[level].second] == 4) { //4번 CCTV인 경우
+            cycle = 4;
+            dir.push_back({-1, 0}), dir.push_back({0, 1}), dir.push_back({0, -1});
+        } else if (board[cctv[level].first][cctv[level].second] == 5) { //5번 CCTV인 경우
+            cycle = 1;
+            dir.push_back({-1, 0}), dir.push_back({0, 1}), dir.push_back({1, 0}), dir.push_back({0, -1});
+        }
+
+        int tmpboard[MAX][MAX];
+        for (int i = 0; i < cycle; i++) {
+            memcpy(tmpboard, b, sizeof tmpboard);
+            //방향 값들을 회전 시킨다.
+            rotate(dir);
+            //보드의 방향으로 감시영역을 체크해준다.
+            check(level, tmpboard, dir);
+            //다음 CCTV에서 탐색이 가능한 경우를 위해 DFS 탐색에 들어간다.
+            DFS(level + 1, tmpboard, DFS);
+        }
+        return;
+    };
+
+    DFS(0, board, DFS);
+
+    cout << ret << "\n";
+    return 0;
 }
+
+//#include<bits/stdc++.h>
+//#define MAX 8
+//#define BLOCK 6
+//using namespace std;
+//
+//int n, m;
+//vector <pair<pair<int, int>, int> > input;
+//int board[MAX][MAX];
+//int ret = INT_MAX;
+//int direction [5][4][2] =
+//{
+//	{ { 0, 1 } },
+//	{ { 0, -1 }, { 0, 1 } },
+//	{ { -1, 0 }, { 0, 1 } },
+//	{ { -1, 0 }, { 0, 1 }, { 0, -1 } },
+//	{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } }
+//};
+//
+//void search(int level, int board[][MAX])
+//{
+//	if (input.size() == level)
+//	{
+//		int cnt = 0;
+//
+//		for (int i = 0; i < n; i++)
+//		for (int j = 0; j < m; j++)
+//		{
+//			if (board[i][j] == 0)
+//				cnt++;
+//		}
+//		ret = min(ret, cnt);
+//		return;
+//	}
+//
+//	int cctvNum = input[level].second;
+//	int cycleValue;
+//	int cursor;
+//	switch (cctvNum)
+//	{
+//	case 0:
+//		cycleValue = 4;
+//		cursor = 1;
+//		break;
+//	case 2:
+//		cycleValue = 4;
+//		cursor = 2;
+//		break;
+//	case 3:
+//		cycleValue = 4;
+//		cursor = 3;
+//		break;
+//	case 1:
+//		cycleValue = 2;
+//		cursor = 2;
+//		break;
+//	default:
+//		cycleValue = 1;
+//		cursor = 4;
+//		break;
+//	}
+//
+//	for (int i = 0; i < cycleValue; i++)
+//	{
+//		int y = input[level].first.first;
+//		int x = input[level].first.second;
+//
+//
+//		int tmpboard[MAX][MAX];
+//		memcpy(tmpboard, board, sizeof(tmpboard));
+//		for (int j = 0; j < cursor; j++)
+//		{
+//			int nextY = y + direction[cctvNum][j][0];
+//			int nextX = x + direction[cctvNum][j][1];
+//			while (1)
+//			{
+//				if (nextY >= 0 && nextY < n && nextX >= 0 && nextX < m)
+//				{
+//					if (tmpboard[nextY][nextX] != 6)
+//						tmpboard[nextY][nextX] = '#';
+//					else
+//						break;
+//				}
+//				else
+//					break;
+//
+//				nextY += direction[cctvNum][j][0];
+//				nextX += direction[cctvNum][j][1];
+//			}
+//		}
+//
+//		search(level + 1, tmpboard);
+//
+//		for (int i = 0; i < cursor; i++)
+//		{
+//			int tmp = - 1 * direction[cctvNum][i][0];
+//			direction[cctvNum][i][0] = direction[cctvNum][i][1];
+//			direction[cctvNum][i][1] = tmp;
+//		}
+//	}
+//}
 
 int main(int argc, char *argv[])
 {
--- 백준알고리즘/구현(Simulation/15684번(사디리조작).cpp
+++ 백준알고리즘/구현(Simulation/15684번(사디리조작).cpp
@@ -1,4 +1,85 @@
 #include<bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define mp(X, Y) make_pair(X,Y)
+#define mt(X, Y) make_tuple(X,Y)
+#define mtt(X, Y, Z) make_tuple(X,Y,Z)
+#define ll long long
+#define sz(v) (int)(v).size()
+
+using namespace std;
+const int MAXN = 11;
+const int MAXH = 31;
+int board[MAXH][MAXN];
+int n, m, h;
+int boardCnt;
+bool flag;
+
+int main(void) {
+    fastio;
+    cin >> n >> m >> h;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        board[a - 1][b] = true;
+    }
+
+    auto DFS = [&](int y, int cnt, auto&& DFS) -> void{
+        if (flag)
+            return;
+
+        if (boardCnt == cnt) {
+            bool possibleFlag = true;
+            //맨 좌측 col부터 시작해서 맨 우측 col까지 사다리를 타면서 내려간다.
+            for (int i = 0; i < n; i++) {
+                int row = i;
+                for (int j = 0; j < h; j++)
+                    //오른쪽으로 사다리를 놓은 경우라면
+                    if (board[j][row])
+                        row++;//출구 위치 증가
+                        //좌측으로 사다리가 놓여져 있는 경우라면
+                        else if (1 < row && board[j][row - 1])
+                            row--;//출구 위치 감소
+
+                            //출구 위치와 사다리를 타기 시작한 i위치가 다르다면 멈춤
+                            if (i != row) {
+                                possibleFlag = false;
+                                break;
+                            }
+            }
+            //출구 위치와 사다리를 타기 시작한 위치가 true라면 참
+            if (possibleFlag)
+                flag = true;
+            return;
+        }
+        for(int i = y; i < h; i++){
+            for(int j = 1; j <n; j++){
+                //2개의 가로선이 연속하거나 서로 접하면 안된다.
+                if(!board[i][j -1] && !board[i][j] && !board[i][j + 1]){
+                    board[i][j] = true;
+                    DFS(i, cnt + 1, DFS);
+                    board[i][j] = false;
+                }
+            }
+        }
+        return;
+    };
+
+    for (int i = 0; i <= 3; i++) {
+        boardCnt = i;
+        DFS(0, 0, DFS);
+        if (flag) {
+            cout << boardCnt << "\n";
+            return 0;
+        }
+    }
+    cout << -1 << "\n";
+}
+
+/*
+#include<bits/stdc++.h/*>
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define pill pair<int,int>
 #define mp(X,Y) make_pair(X,Y)
@@ -76,7 +157,7 @@ int main(void)
 		}
 	}
 	cout << -1 << "\n";
-}
+}*/
 /*
 #include<bits/stdc++.h>
 #define MAX 30
